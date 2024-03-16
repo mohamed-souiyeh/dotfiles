5d020394a445ac2a19c54cd430ac57eb61a07e87
mohamed-souiyeh <mohamedsouiyeh3@gmail.com>
2024-03-16 03:18:21 +0000

    added auto-save to nvim and finished zellij config

---
+++ configs/nvim/.config/nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,20 @@
+return {
+  'https://git.sr.ht/~nedia/auto-save.nvim',
+  event = { 'BufReadPre' },
+  opts = {
+    events = { 'InsertLeave', 'BufLeave', 'ModeChanged', 'BufHidden' },
+    silent = true,
+    timeout = 300,
+    save_fn = function()
+      local config = require 'auto-save.config'
+      if config.save_cmd ~= nil then
+        vim.cmd(config.save_cmd)
+      elseif config.silent then
+        vim.cmd 'silent! wa'
+      else
+        vim.cmd 'wa'
+      end
+    end,
+    exclude_ft = { 'neo-tree' },
+  },
+}
--- configs/nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ configs/nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -196,6 +196,7 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'tsserver',
         'stylua', -- Used to format lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
